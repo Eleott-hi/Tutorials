a5e586dbd15b11d118cfb95634578882d433cbed
pintoved <laptev18vv@gmail.com>
2024-05-13 13:36:53 +0700

    K8s + alembic

---
+++ K8s/src/install.sh
@@ -0,0 +1,1 @@
+curl -Ls "https://sbom.k8s.io/$(curl -Ls https://dl.k8s.io/release/stable.txt)/release" | grep "SPDXID: SPDXRef-Package-registry.k8s.io" |  grep -v sha256 | cut -d- -f3- | sed 's/-/\//' | sed 's/-v1/:v1/'
